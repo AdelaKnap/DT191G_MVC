0d15b53e963dc14e4e303e53317dec5c96f5253a
Adela Knap <adkn2300@student.miun.se>
2025-01-30 14:22:01 +0100

    Lägger till json.filer och formulär mm

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,9 +8,12 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+
+        // Läsa in JSON-fil och deserialisera
         var jsonStr = System.IO.File.ReadAllText("dogs.json");
         var dogs = JsonSerializer.Deserialize<List<DogModel>>(jsonStr);
-        return View(dogs);
+
+        return View(dogs);  // Skicka med listan till vyn
     }
 
     [Route("/om")]
@@ -19,9 +22,44 @@ public class HomeController : Controller
         return View();
     }
 
-    [Route("/raser")]
+    [Route("/hundar")]
     public IActionResult Breeds()
     {
+        // Läs in form.json och deserialisera
+        string jsonStr = System.IO.File.ReadAllText("form.json");
+        var form = JsonSerializer.Deserialize<List<FormModel>>(jsonStr);
+
+        return View(form);  // Skicka med listan till vyn
+    }
+
+    [HttpPost]
+    [Route("/om")]
+    public IActionResult About(FormModel model)
+    {
+        // Validera data
+        if (ModelState.IsValid)
+        {
+
+            // Läs in form.json och deserialisera
+            string jsonStr = System.IO.File.ReadAllText("form.json");
+            var form = JsonSerializer.Deserialize<List<FormModel>>(jsonStr);
+
+            // Lägg till i form.json
+            if (form != null)
+            {
+                form.Add(model);
+                //Serialisera och spara till form.json
+                jsonStr = JsonSerializer.Serialize(form);
+                System.IO.File.WriteAllText("form.json", jsonStr);
+            }
+
+            ViewData["Message"] = "Hund tillagd!";  // Meddelande om tillagd
+            ModelState.Clear();                     // rensa formuläret
+
+            return RedirectToAction("Breeds", "Home");    // Redirect till underisdan hundar
+        }
+
         return View();
     }
+
 }
---
+++ Models/FormModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DT191G_MVC.Models;
+
+public class FormModel
+{
+    // Properties
+    [Required(ErrorMessage = "Du måste ange hundens namn.")]
+    [Display(Name = "Hundens namn:")]
+    public string? DogName { get; set; }
+
+    [Required(ErrorMessage = "Du måste ange ras.")]
+    [Display(Name = "Ras:")]
+    public string? BreedName { get; set; }
+
+    [Required(ErrorMessage = "Du måste ange ålder.")]
+    [Range(0, 30, ErrorMessage = "Åldern måste vara mellan 0 och 30.")]
+    [Display(Name = "Ålder:")]
+    public int? Age { get; set; }
+
+    // [Required]
+    // public required string Color { get; set; }    // Dropdown
+
+    [Required(ErrorMessage = "Du måste välja om du är en hund-nörd eller inte.")]
+    public bool? DogLover { get; set; }
+}
